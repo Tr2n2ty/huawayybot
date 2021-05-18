f674b08992bf1ec88a25f3bac63459b81acca1e2
huaWayy <51531323+Tr2n2ty@users.noreply.github.com>
2021-05-18 15:50:31 +0200

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'server',
-  description: 'Server informations.',
-  execute(message) {
-    message.channel.send(`Server Name: ${message.guild.name}\nUser count : ${message.guild.memberCount}\nOwner : <@470317438203068416>`);
-  }
-};
