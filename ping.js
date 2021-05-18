d31e80319570a7c427e3d8032f9aca1fe2fb18e5
huaWayy <51531323+Tr2n2ty@users.noreply.github.com>
2021-05-18 15:50:54 +0200

    Delete ping.js

--- ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping test',
-    execute(message) {
-        message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    }
-}
